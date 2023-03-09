5b1661df9f8610ddeebce8c9c25db80811e00312
artdm lab <artdm@dvc.edu>
2023-03-09 12:14:30 -0800

    new

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded');
 
-function init(){
-    const back = document.querySelector(".back-btn");
-    const next = document.querySelector(".next_btn");
-    const slides = document.querySelector(".slides");
+const dinos = [document.querySelectorAll(".slide")]
+const back = document.querySelector(".back-btn");
+const next = document.querySelector(".next_btn");
+const slides = document.querySelector(".slides");
+let index = 0;
 
+next.onClick = function (e) {
 
-    slides.forEach((slide) => {
-        slide.classList.add("hide");
-        
-    });
+    
 }
+
+
+
+
+
+
+
