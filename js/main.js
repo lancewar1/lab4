aae36b672f011afe80639a055da607b8d5d95cec
Lance Warshawsky <lance.warshawsky@gmail.com>
2023-03-12 13:58:47 -0700

    updated js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,54 @@
-document.addEventListener('DOMContentLoaded');
+document.addEventListener('DOMContentLoaded', init);
 
-const dinos = [document.querySelectorAll(".slide")]
-const back = document.querySelector(".back-btn");
-const next = document.querySelector(".next_btn");
-const slides = document.querySelector(".slides");
-let index = 0;
+function init(){
+const back_btn = document.querySelector(".back-btn");
+const next_btn = document.querySelector(".next-btn");
+const frame = document.querySelector(".frame");
+const slides = frame.querySelectorAll("img");
 
-next.onClick = function (e) {
+slides.forEach((slide) => {
+    slide.classList.add("hide");
+  });
 
-    
+
+  slides[0].classList.remove("hide");
+
+  next_btn.addEventListener("click",changeSlide);
+  back_btn.addEventListener("click", changeSlide);
 }
 
+function changeSlide(e){
 
+    e.preventDevault();
 
+    const frame = document.querySelector(".frame");
+    const slides = frame.querySelectorAll("img");
+    let showing = document.querySelector(".current");
+    let nextUp = "";
 
+    if(e.target.className == 'next-btn') {
+        nextUp = showing.nextElementSibling;
+      }
+    
+      if(e.target.className == 'back-btn') {
+        nextUp = showing.previousElementSibling;
+      }
+      
+     
+      showing.classList.toggle("hide");
+      showing.classList.toggle("current");
+      
+     
+      if (!nextUp) {
+        nextUp = slides[slides.length - 1];
+      }
+    
+      if (nextUp.nodeName !== "IMG") {
+        nextUp = slides[0];
+      }
+    
+     
+      nextUp.classList.toggle("hide");
+      nextUp.classList.toggle("current");
 
-
-
+}
